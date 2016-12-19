b091b3fffece7c3b9034aadb9c472235ba685ffe
Travis Marble <travis.marble@centeva.com>
2016-12-19 15:42:30 -0700

    cleanup demo app

--- demo/app/component/home/home.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var home_component_1 = require("./home.component");
-exports.HomeRoutes = [
-    {
-        path: 'home',
-        component: home_component_1.HomeComponent
-    }
-];
-//# sourceMappingURL=home.routes.js.map
